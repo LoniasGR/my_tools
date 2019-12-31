2de7338f9cfccc775840ac412c1512ff3d9d3608
Leonidas Avdelas <avdelasleonidas@gmail.com>
2019-12-31 05:11:51 +0200

    Added debian support

--- install_python_lab.sh
+++ install_python_lab.sh
@@ -1,54 +1,101 @@
 #!/usr/bin/env bash
 
-## This is for CentOS server. Adapt accordingly.
+case $1 in
+    -all)       install=0;;
+    -basic)     install=1;;
+    -conda)     install=2;;
+    -condafst)  install=3;;
+    -openfst)   install=4;;
+    -fstkaldi)  install=5;;
+    -kaldi)     install=6;;
+esac
 
-## Make sure you have the latest version.
-sudo dnf -y check-update
-sudo dnf -y update
+release="$(tail -n 1 /etc/system-release)"
+case "${release}" in
+    CentOS*)    distro=CentOS;;
+    Ubuntu*)    distro=Ubuntu;;
+    Debian*)    distro=Debian;;
+    *)      distro="Other";
+esac
 
-## Install Extra Packages for Enterprise Linux repository
-sudo dnf -y install epel-release
+echo 'Linux distribution is' ${release}
 
-## Enable PowerTools repository
-sudo dnf -y config-manager --set-enabled PowerTools
-
-## Download and install the CERT Forensics tools rpm
-sudo dnf -y install wget
+# Change to HOME
 cd ~
-wget https://forensics.cert.org/cert-forensics-tools-release-el8.rpm
-sudo rpm -i ./cert-forensics-tools-release-el8.rpm
-rm ./cert-forensics-tools-release-el8.rpm
 
-## Make sure all repos are up-to-date
-sudo dnf -y check-update
-sudo dnf -y update
+# Get basic dependencies
+if [ "${install}" -lt "2" ]; then 
 
-## Install important packages
-sudo dnf install -y patch bzip2 curl git moreutils gawk tmux htop python2 gcc gcc-c++ gcc-gfortran make platform-python-devel zlib-devel zip automake autoconf sox libtool subversion
+    # CentOS server - my default
+    if [ "${distro}" = "CentOS" ]; then
 
-## Install miniconda
-bash install_miniconda.sha
+        ## Make sure you have the latest version.
+        sudo dnf -y check-update
+        sudo dnf -y update
 
-## Install important python packages
-conda install -y numpy matplotlib jupyter jupyterlab scikit-learn nltk scipy pandas scikit-image tqdm
-conda install -y -c conda-forge visdom librosa seaborn
+        ## Install Extra Packages for Enterprise Linux repository
+        sudo dnf -y install epel-release
 
-## Install important NLTK libraries
-python -m nltk.downloader popular
+        ## Enable PowerTools repository
+        sudo dnf -y config-manager --set-enabled PowerTools
 
-## Install pytorch no-CUDA
-conda install -y pytorch torchvision cpuonly -c pytorch
+        ## Download and install the CERT Forensics tools rpm
+        sudo dnf -y install wget
+        cd ~
+        wget https://forensics.cert.org/cert-forensics-tools-release-el8.rpm
+        sudo rpm -i ./cert-forensics-tools-release-el8.rpm
+        rm ./cert-forensics-tools-release-el8.rpm
 
-## Install open-fst
-NUM_CPUS=`cat /proc/cpuinfo | grep proc | wc -l`
-OPENFST_VERSION=1.6.1
+        ## Make sure all repos are up-to-date
+        sudo dnf -y check-update
+        sudo dnf -y update
 
-echo 'Working with ' ${NUM_CPUS} ' CPUs.'
-echo 'Installing OpenFST.'
-sleep 15
-bash install_openfst.sh ${NUM_CPUS} ${OPENFST_VERSION}
+        ## Install important packages
+        sudo dnf install -y patch bzip2 curl git moreutils gawk tmux htop python2 gcc gcc-c++ gcc-gfortran make platform-python-devel zlib-devel zip automake autoconf sox libtool subversion 
+    fi
 
-## Install kaldi
-echo 'Installing kaldi.'
-sleep 15
-bash install_kaldi.sh ${NUM_CPUS}
+    # Debian server - the wise choice
+    if [ "${distro}" = "Ubuntu" ] || [ "${distro}" = "Debian" ]; then
+
+        ## Update everything to latest version
+        sudo apt-get update
+        sudo apt-get upgrade
+
+        sudo apt-get install -y patch bzip2 curl git moreutils gawk tmux htop python2 gcc g++ make python-dev libz-dev zip automake autoconf sox libtool subversion wget gfortran zlib1g-dev checkinstall
+    fi
+fi
+
+
+if [ "${install}" -lt "4" -a "${install}" -ne "2" ]; then 
+    ## Install miniconda
+    bash handy_tools/install_miniconda.sh
+
+    ## Install important python packages
+    conda install -y numpy matplotlib jupyter jupyterlab scikit-learn nltk scipy pandas scikit-image tqdm
+    conda install -y -c conda-forge visdom librosa seaborn
+
+    ## Install important NLTK libraries
+    python -m nltk.downloader popular
+
+    ## Install pytorch no-CUDA
+    conda install -y pytorch torchvision cpuonly -c pytorch
+fi
+
+
+if [ "${install}" -lt "6" -a "${install}" -ne "2" -a "${install}" -ne "1" ]; then 
+    ## Install open-fst
+    NUM_CPUS=`cat /proc/cpuinfo | grep proc | wc -l`
+    OPENFST_VERSION=1.6.1
+
+    echo 'Working with ' ${NUM_CPUS} ' CPUs.'
+    echo 'Installing OpenFST.'
+    sleep 15
+    bash handy_tools/install_openfst.sh ${NUM_CPUS} ${OPENFST_VERSION}
+fi
+
+if [ "${install}" -lt "7" -a "${install}" -ne "2" -a "${install}" -ne "1" -a "${install}" -ne "4" ]; then 
+    ## Install kaldi
+    echo 'Installing kaldi.'
+    sleep 15
+    bash handy_tools/install_kaldi.sh ${NUM_CPUS}
+fi
